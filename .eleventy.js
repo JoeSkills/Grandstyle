0f7905edb9a01196a3682de6d6d5a91b5238f778
Joseph Oritseweyinmi <josephoritseweyinmi5@gmail.com>
2023-12-05 01:11:05 +0100

    Made some updates

--- .eleventy.js
+++ .eleventy.js
@@ -38,7 +38,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/admin');
 
   eleventyConfig.addFilter('postDate', (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED``);
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
   return {
