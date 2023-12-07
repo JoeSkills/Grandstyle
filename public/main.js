55695aba8cea6693fd884b2fe35a592fb284261f
Joseph Oritseweyinmi <josephoritseweyinmi5@gmail.com>
2023-12-07 22:48:05 +0100

    Made some updates again

--- public/main.js
+++ public/main.js
@@ -5,14 +5,7 @@ const swiper = new Swiper('.swiper', {
   },
   grabCursor: true,
   loop: true,
-  on: {
-    slideNextTransitionEnd: (swiper) => {
-      changeTextAnimation(1);
-    },
-    slidePrevTransitionEnd: (swiper) => {
-      changeTextAnimation(-1);
-    },
-  },
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -60,56 +53,3 @@ document.addEventListener('DOMContentLoaded', function () {
     observer.observe(section);
   });
 });
-
-const changingData = [
-  {
-    h1: 'Teesol',
-    p: 'Immerse yourself in the world of rhythm and creativity',
-    img: './assets/teesol-1.JPG',
-    link: '/discography/',
-    linkText: 'Get Access To Top Music',
-  },
-
-  {
-    h1: 'Experience the Beat',
-    p: 'Discover new sounds with GrandStyle Entertainment',
-    img: './assets/services-image-1.jpeg',
-    link: '/services/',
-    linkText: 'Get To Know More',
-  },
-
-  {
-    h1: 'Prince Gadaffi Gold',
-    p: 'Immerse yourself in the world of rhythm and creativity',
-    img: './assets/prince-gad.JPG',
-    link: '/discography/',
-    linkText: 'Get Access To Top Music',
-  },
-];
-
-let currentDataIndex = 0;
-const changingH1 = document.querySelector('h1.changing-text');
-const changingP = document.querySelector('p.changing-text');
-const changingButton = document.querySelector('.changing-button');
-const headerCtaBtn = document.querySelector('.header-cta-btn');
-const changingWrapperEl = document.querySelector('.front-text');
-
-function changeTextAnimation(animationVal = 1) {
-  const currentData = changingData[currentDataIndex];
-  changingH1.textContent = currentData.h1;
-  changingP.textContent = currentData.p;
-  changingButton.textContent = currentData.linkText;
-  changingButton.href = currentData.link;
-  currentDataIndex = (currentDataIndex + animationVal) % changingData.length;
-}
-
-function checkAnimationProgress() {
-  const computedStyle = getComputedStyle(changingWrapperEl);
-  const opacity = parseFloat(computedStyle.opacity);
-
-  headerCtaBtn.style.display = opacity === 0 ? 'block' : 'none';
-
-  requestAnimationFrame(checkAnimationProgress);
-}
-
-checkAnimationProgress();
